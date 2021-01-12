dab10e0ff080727ff01730ba33385415f6759cda
consoledot <bimbooladele@outlook.com>
2021-01-12 13:58:54 +0100

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Welcome to</h2>
+        <h1>The Shoppies: Movie awards for entrepreneurs</h1>
+        <p>Another award season is here, nominate your top 5 movies of 2020</p>
+        <a>Nominate</a>
     </div>
   );
 }
